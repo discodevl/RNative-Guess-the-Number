41de0319d9271b63b6f0fdc1f52753a3ffb7978f
discodevl <donato.cutro3@gmail.com>
2022-07-14 00:06:45 +0200

    lesson 52

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,10 @@
-import { TextInput, View } from "react-native";
+import { TextInput, View, StyleSheet } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 function StartGameScreen() {
   return (
-    <View>
-      <TextInput />
+    <View style={styles.inputContainer}>
+      <TextInput style={styles.numberInput} keyboardType="number-pad" maxLength={99}/>
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -12,3 +12,28 @@ function StartGameScreen() {
 }
 
 export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: '#4e0329',
+    borderRadius: 8,
+    elevation: 24, //box-shadow
+    //shadowColor: 'black', ios props
+    //shadowOpacity: 1
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center',
+
+  }
+})
